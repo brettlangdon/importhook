123cea4360881ce79fc9dfa1585963425f6e3d26
brettlangdon <me@brett.is>
2018-08-25 19:11:05 -0400

    v1.0.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_long_description():
 
 setup(
     name='importhook',
-    version='1.0.4',
+    version='1.0.5',
     url='https://github.com/brettlangdon/importhook',
     license='MIT',
     author='Brett Langdon',
