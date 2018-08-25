9f970bce8cc911e345ce82ab01f86a413a49bf15
brettlangdon <me@brett.is>
2018-08-24 20:07:34 -0400

    Bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_long_description():
 
 setup(
     name='importhook',
-    version='1.0.1',
+    version='1.0.2',
     url='https://github.com/brettlangdon/importhook',
     license='MIT',
     author='Brett Langdon',
